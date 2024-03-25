ca56ad4af993d1cefea2b7f44ac6620b51a68a01
Manik <tonysins43@gmail.com>
2024-03-25 08:29:25 +0600

    make AppError for show error

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -3,27 +3,32 @@ import catchAsync from "../utils/catchAsync";
 import jwt from "jsonwebtoken";
 import verifyToken from "../helpers/verifyToken";
 import config from "../config";
+import AppError from "../errors/appError";
+import httpStatus from "http-status";
 
 const auth = (...requiredRoles: string[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req?.headers?.authorization;
     if (!token) {
-      throw new Error("You are not authorized to access this");
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        "You are not authorized to access this"
+      );
     }
     let decoded;
 
     try {
       decoded = verifyToken(token, config.jwt_access_secret as string);
     } catch (err) {
-      throw new Error("Token is expired");
+      throw new AppError(httpStatus.UNAUTHORIZED, "Token is expired");
     }
     if (!decoded) {
-      throw new Error("Token is expired");
+      throw new AppError(httpStatus.UNAUTHORIZED, "Token is expired");
     }
     const { role, userId, iat } = decoded;
 
     if (requiredRoles && !requiredRoles.includes(role)) {
-      throw new Error("Your are not authorized");
+      throw new AppError(httpStatus.UNAUTHORIZED, "You are un authorized");
     }
     next();
   });
