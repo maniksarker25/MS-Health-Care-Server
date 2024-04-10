94f377bec004773b9522d9b5eb076a9f32c90059
Manik <tonysins43@gmail.com>
2024-04-10 16:47:36 +0600

    make schema for medical report and patient health data

---
+++ prisma/migrations/20240410104712_health_data_and_medical_report/migration.sql
@@ -0,0 +1,52 @@
+-- CreateEnum
+CREATE TYPE "BloodGroup" AS ENUM ('A_POSITIVE', 'B_POSITIVE', 'O_POSITIVE', 'AB_POSITIVE', 'A_NEGATIVE', 'B_NEGATIVE', 'O_NEGATIVE', 'AB_NEGATIVE');
+
+-- CreateEnum
+CREATE TYPE "MaritalStatus" AS ENUM ('MARRIED', 'UNMARRIED');
+
+-- CreateTable
+CREATE TABLE "patientHealthDatas" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "dateOfBirth" TEXT NOT NULL,
+    "bloodGroup" "BloodGroup" NOT NULL,
+    "hasAllergies" BOOLEAN DEFAULT false,
+    "hasDiabetes" BOOLEAN DEFAULT false,
+    "height" TEXT NOT NULL,
+    "weight" TEXT NOT NULL,
+    "smokingStatus" BOOLEAN DEFAULT false,
+    "dietaryPreferences" TEXT,
+    "pregnancyStatus" BOOLEAN DEFAULT false,
+    "mentalHealthHistory" TEXT,
+    "immunizationStatus" TEXT,
+    "hasPastSurgeries" BOOLEAN DEFAULT false,
+    "recentAnxiety" BOOLEAN DEFAULT false,
+    "recentDepression" BOOLEAN DEFAULT false,
+    "maritalStatus" "MaritalStatus" NOT NULL DEFAULT 'UNMARRIED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "patientHealthDatas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "medicalReport" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "reportName" TEXT NOT NULL,
+    "reportLink" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "medicalReport_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "patientHealthDatas_patientId_key" ON "patientHealthDatas"("patientId");
+
+-- AddForeignKey
+ALTER TABLE "patientHealthDatas" ADD CONSTRAINT "patientHealthDatas_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "medicalReport" ADD CONSTRAINT "medicalReport_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
