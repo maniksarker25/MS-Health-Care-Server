445e668e959bf0fbc48020d5b1b2eb69cdd4b8a7
Manik <tonysins43@gmail.com>
2024-04-16 21:37:58 +0600

    update global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,16 +1,46 @@
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from "@prisma/client/runtime/library";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
+// const globalErrorHandler = (
+//   err: any,
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//     success: false,
+//     message: err.message || "Something went wrong",
+//     error: err,
+//   });
+// };
 const globalErrorHandler = (
   err: any,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: err.message || "Something went wrong",
-    error: err,
+  let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = "Something went wrong";
+  let error = err;
+  if (err instanceof PrismaClientValidationError) {
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+    message = "Validation error";
+    error = err.message;
+  } else if (err instanceof PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      message = "Duplicate key error";
+      error = err.meta;
+    }
+  }
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
   });
 };
 
